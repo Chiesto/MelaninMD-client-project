9225a58a64c4577b2d02981217312fa357730eb6
Mahamud Aden <mahamudadn@gmail.com>
2023-08-10 10:20:57 -0500

    added comments

--- server/routes/condition.router.js
+++ server/routes/condition.router.js
@@ -1,109 +1,109 @@
-const express = require('express');
-const router = express.Router();
-const FormData = require('form-data');
-const axios = require('axios');
-//const { request } = require('http');
-require('dotenv').config();
-const stream = require('stream').Readable;
-const pool = require('../modules/pool');
+    const express = require('express');
+    const router = express.Router();
+    const FormData = require('form-data');
+    const axios = require('axios');
+    //const { request } = require('http');
+    require('dotenv').config();
+    const stream = require('stream').Readable;
+    const pool = require('../modules/pool');
 
 
 
-//setting up the diagnosis ID outside of everything so that it can be changed as needed
-let diagnosisId;
+    //setting up the diagnosis ID outside of everything so that it can be changed as needed
+    let diagnosisId;
+
+    // Handle POST reques
+    router.post('/',  (req, res) => {
+        const apiUrl = "https://autoderm.firstderm.com/v1/query";
 
 
-router.post('/',  (req, res) => {
-    const apiUrl = "https://autoderm.firstderm.com/v1/query";
+        // const imageUrl = 'https://assets.nhs.uk/nhsuk-cms/images/S_0917_acne_M1080444.max-600x600.jpg';
 
 
-    // const imageUrl = 'https://assets.nhs.uk/nhsuk-cms/images/S_0917_acne_M1080444.max-600x600.jpg';
+        // Check if files were uploaded
+        if (!req.files || Object.keys(req.files).length === 0) {
+            return res.status(400).send('No files were uploaded.');
+        }
+    // Get image data from request
+        const imageBuffer = req.files.image.data;
 
-
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).send('No files were uploaded.');
-    }
-
-    const imageBuffer = req.files.image.data;
-
-    
-    // make sure image key is also on front end code.
-    const uploadedFile = req.files?.image;
-    console.log('outthewazoo',uploadedFile);
-    
-    
-    //FILESTACK DOES NOT ACCEPT JPEG
-    const apiKey2 = process.env.API_KEY;
-    const fileStackKey = process.env.FILE_STACK_API_KEY;
-
-          axios({
-            method: "POST",
-            url: `https://www.filestackapi.com/api/store/S3?key=${fileStackKey}`,
-            data: imageBuffer,
-            headers: {
-                "Content-Type": "image/*"
-            }
-        }).then(response => {
-            const displayUrl = response.data.url + '?dl=false';
-
-            console.log('taco', response.data, 'tacoURL',response.data?.url)
-
-            axios.get(displayUrl, { responseType: 'arraybuffer' }).then((imageResponse) => {
-                const imageData = new Buffer.from(imageResponse.data, 'binary');
-                const formData = new FormData();
-                const bufferStream = new stream.PassThrough();
-                bufferStream.end(imageData);
-                formData.append('file', bufferStream, { filename: response.data.filename, contentType: 'image/*' });
         
-                const formHeaders = formData.getHeaders();
-                formHeaders['Api-Key'] = apiKey2;
-        
-                return axios.post(apiUrl, formData, { headers: formHeaders });
-            }).then((response) => {
-                const date = new Date();
-                console.log(response.data);
-                const resp = {url: displayUrl, predictions: response.data.predictions}
-                const predictions = response.data.predictions;
-                
+        // make sure image key is also on front end code.
+        const uploadedFile = req.files?.image;
+    
+         // API keys
+        //FILESTACK DOES NOT ACCEPT JPEG
+        const apiKey2 = process.env.API_KEY;
+        const fileStackKey = process.env.FILE_STACK_API_KEY;
 
-                let queryText = 'INSERT INTO diagnosis (user_id) VALUES ($1) RETURNING id';
-                let values = [req.user.id];
-                pool.query(queryText, values)
-                    .then(response =>{
-                        console.log('insert into diagnosis WORKS! ',response.rows)
-                        diagnosisId = response.rows[0].id;
-                        queryText = 'INSERT INTO pictures (diagnosis_id, filepath, date) VALUES ($1, $2, $3)';
-                        values = [diagnosisId, displayUrl, date];
-                        pool.query(queryText, values)
-                            .then(response2=>{
-
-                                console.log('insert into pictures works!',response2);
-                                for (let prediction of predictions){
-                                    queryText = `INSERT INTO prediction (diagnosis_id, prediction, icd, classification_id, link, confidence)
-                                                VALUES ($1, $2, $3, $4, $5, $6)`
-                                    values = [diagnosisId, prediction.name, prediction.icd, prediction.classificationId, prediction.readMoreUrl, prediction.confidence]
-                                    pool.query(queryText, values)
-                                }
-                            }).then(response3=>{
-                                console.log('all DB queries worked!', response3);
-                                res.send(resp)
-
-                            })
-
-                    })
-
-                
-            }).catch((error) => {
-                console.error('prolemas =>',error);
-                if (error.response) {
-                    console.error(`Response data =>>>>>>> ${error.response.data}`);
-                    console.error(`Response status $$$$$$$$$ ${error.response.status}`);
+            // Store image using Filestack API
+            axios({
+                method: "POST",
+                url: `https://www.filestackapi.com/api/store/S3?key=${fileStackKey}`,
+                data: imageBuffer,
+                headers: {
+                    "Content-Type": "image/*"
                 }
-                res.sendStatus(500);
+            }).then(response => {
+                const displayUrl = response.data.url + '?dl=false';
+
+                 // Retrieve image data and create a FormData
+                axios.get(displayUrl, { responseType: 'arraybuffer' }).then((imageResponse) => {
+                    const imageData = new Buffer.from(imageResponse.data, 'binary');
+                    const formData = new FormData();
+                    const bufferStream = new stream.PassThrough();
+                    bufferStream.end(imageData);
+                    formData.append('file', bufferStream, { filename: response.data.filename, contentType: 'image/*' });
+            // Sets headers for API request
+                    const formHeaders = formData.getHeaders();
+                    formHeaders['Api-Key'] = apiKey2;
+            
+                    return axios.post(apiUrl, formData, { headers: formHeaders });
+                }).then((response) => {
+                    const date = new Date();
+                    console.log(response.data);
+                    const resp = {url: displayUrl, predictions: response.data.predictions}
+                    const predictions = response.data.predictions;
+                    
+
+                    let queryText = 'INSERT INTO diagnosis (user_id) VALUES ($1) RETURNING id';
+                    let values = [req.user.id];
+                    pool.query(queryText, values)
+                        .then(response =>{
+                            console.log('insert into diagnosis WORKS! ',response.rows)
+                            diagnosisId = response.rows[0].id;
+                            queryText = 'INSERT INTO pictures (diagnosis_id, filepath, date) VALUES ($1, $2, $3)';
+                            values = [diagnosisId, displayUrl, date];
+                            pool.query(queryText, values)
+                                .then(response2=>{
+
+                                    console.log('insert into pictures works!',response2);
+                                    for (let prediction of predictions){
+                                        queryText = `INSERT INTO prediction (diagnosis_id, prediction, icd, classification_id, link, confidence)
+                                                    VALUES ($1, $2, $3, $4, $5, $6)`
+                                        values = [diagnosisId, prediction.name, prediction.icd, prediction.classificationId, prediction.readMoreUrl, prediction.confidence]
+                                        pool.query(queryText, values)
+                                    }
+                                }).then(response3=>{
+                                    console.log('all DB queries worked!', response3);
+                                    res.send(resp)
+
+                                })
+
+                        })
+
+                    
+                }).catch((error) => {
+                    console.error('prolemas =>',error);
+                    if (error.response) {
+                        console.error(`Response data =>>>>>>> ${error.response.data}`);
+                        console.error(`Response status $$$$$$$$$ ${error.response.status}`);
+                    }
+                    res.sendStatus(500);
+                });
+            
             });
-        
-        });
-        })
+            })
 
 
-module.exports = router;
+    module.exports = router;
--- src/components/Prediction/Prediction.jsx
+++ src/components/Prediction/Prediction.jsx
@@ -1,151 +1,168 @@
 import { useEffect } from "react";
 import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import './Prediction.css';
+import "./Prediction.css";
 import { Bar } from "react-chartjs-2";
-import {Card , CardContent, Typography, Button} from "@mui/material"
+import { Card, CardContent, Typography, Button } from "@mui/material";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend,
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend,
 } from "chart.js";
 // Register necessary chart elements and plugins
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
+	CategoryScale,
+	LinearScale,
+	BarElement,
+	Title,
+	Tooltip,
+	Legend
 );
-import Backdrop from '@mui/material/Backdrop';
-import CircularProgress from '@mui/material/CircularProgress';
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
 function Prediction() {
-    const conditionReducer = useSelector((store) => store.conditionReducer);
-    const [open, setOpen] = useState(false);
-    function readMore (condition) {
-        window.location.href = condition.readMoreUrl;
-}
-    // const options = {
-    //     scales: {
-    //         y: {        // y axis
-    //             beginAtZero: true,
-    //             title: {
-    //                 display: true,
-    //                 text: "Confidence (%)",
-    //             },
-    //             ticks: {
-    //                 callback: function (value) {
-    //                     return value + "%";
-    //                 },
-    //             },
-    //         },
-    //         x: {    //x axis
-    //             title: {
-    //                 display: true,
-    //                 text: "Predictions",
-    //             },
-    //         },
-    //     },
-    // };
+	const conditionReducer = useSelector((store) => store.conditionReducer);
+	const [open, setOpen] = useState(false);
+	function readMore(condition) {
+		window.location.href = condition.readMoreUrl;
+	}
+	// const options = {
+	//     scales: {
+	//         y: {        // y axis
+	//             beginAtZero: true,
+	//             title: {
+	//                 display: true,
+	//                 text: "Confidence (%)",
+	//             },
+	//             ticks: {
+	//                 callback: function (value) {
+	//                     return value + "%";
+	//                 },
+	//             },
+	//         },
+	//         x: {    //x axis
+	//             title: {
+	//                 display: true,
+	//                 text: "Predictions",
+	//             },
+	//         },
+	//     },
+	// };
 
-    const options = {
-        scales: {
-          y: {
-            beginAtZero: true,
-            title: {
-              display: true,
-            //   text: "Confidence (%)",
-            },
-            ticks: {
-              callback: function (value) {
-                return value + "%";
-              },
-            },
-          },
-          x: {
-            title: {
-              display: true,
-            //   text: "Predictions",
-            },
-            ticks: {
-              autoSkip: false,
-              maxRotation: 30,
-              minRotation: 30,
-            },
-          },
-        },
-      };
-    
-  // Generate chart data based on conditionReducer
-    const generateChartData = () => {
-        const labels =
-            conditionReducer.predictions?.map((condition) => condition.name) || [];
-        const data =
-            conditionReducer.predictions?.map(
-                (condition) => condition.confidence * 100
-            ) || [];
-        return {
-            labels: labels,
-            datasets: [
-                {
+	const options = {
+		scales: {
+			y: {
+				beginAtZero: true,
+				title: {
+					display: true,
+					//   text: "Confidence (%)",
+				},
+				ticks: {
+					callback: function (value) {
+						return value + "%";
+					},
+				},
+			},
+			x: {
+				title: {
+					display: true,
+					//   text: "Predictions",
+				},
+				ticks: {
+					autoSkip: false,
+					maxRotation: 30,
+					minRotation: 30,
+				},
+			},
+		},
+	};
 
-               
+	// Generate chart data based on conditionReducer
+	const generateChartData = () => {
+		const labels =
+			conditionReducer.predictions?.map((condition) => condition.name) || [];
+		const data =
+			conditionReducer.predictions?.map(
+				(condition) => condition.confidence * 100
+			) || [];
+		return {
+			labels: labels,
+			datasets: [
+				{
+					label: "Confidence Level",
 
-                    label: "Confidence Level",
+					data: data,
+					backgroundColor: [
+						"lavender",
+						"#8D9DB2",
+						"#F4C2C2",
+						"#7B7A7A",
+						"rgb(170, 170, 232)",
+						"purple",
+						"pink",
+					], // Customize the bar color here
+				},
+			],
+			borderRadius: 25,
+		};
+	};
 
-                    data: data,
-                    backgroundColor: [
-                        "lavender",
-                        "#8D9DB2",
-                        "#F4C2C2",
-                        "#7B7A7A",
-                        "rgb(170, 170, 232)",
-                        "purple",
-                        "pink",
-                    ], // Customize the bar color here
-                },
-            ],
-            borderRadius: 25,
-        };
-    };
-    
-    return(
-        <>
-        <br />
-        {conditionReducer.predictions ? ( <>
-            <Card className="card-one" elevation={4} style={{ maxWidth: "75%", justifyContent: "center", alignItems: "center", alignContent: "center", marginLeft: "40px" }}>
- 
+	return (
+		<>
+			<br />
+			{conditionReducer.predictions ? (
+				<>
+					<Card
+						className="card-one"
+						elevation={4}
+						style={{
+							maxWidth: "75%",
+							justifyContent: "center",
+							alignItems: "center",
+							alignContent: "center",
+							marginLeft: "40px",
+						}}
+					>
+						{/* <img className="image-container" src={conditionReducer.url}/> */}
+						<CardContent
+							style={{
+								justifyContent: "center",
+								alignItems: "center",
+								alignContent: "center",
+								marginTop: "10px",
+								marginRight: "20px",
+							}}
+						>
+							<img src={conditionReducer.url} />
+						</CardContent>
+					</Card>
+					{conditionReducer.predictions?.map((condition, i) => (
+						<div className="data-container" key={i}>
+							<b className="condition-name">{condition.name}</b>
+							<button className="p-btn" onClick={() => readMore(condition)}>
+								Read More
+							</button>
+							{/* <p>{condition.confidence}</p> */}
+						</div>
+					))}
 
-        {/* <img className="image-container" src={conditionReducer.url}/> */}
-            <CardContent style={{justifyContent: "center", alignItems: "center", alignContent: "center", marginTop: "10px", marginRight: "20px"}}>
-                <img src={conditionReducer.url} />
-            </CardContent>
-        </Card>
-        {conditionReducer.predictions?.map((condition, i) => (
-                <div className="data-container" key={i}>
-                    <b className="condition-name">{condition.name}</b>
-                    <button className="p-btn" onClick={()=> readMore(condition)}>Read More</button>
-                    {/* <p>{condition.confidence}</p> */}
-                </div>
-        ))}
-
-        <div className="bar">
-                <Bar data={generateChartData()} options={options} />
-            </div>
-        </>) :  <Backdrop
-        sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={!conditionReducer.predictions}
-      >
-        <CircularProgress color="inherit" />
-      </Backdrop>
-      }
-        </>
-    )
+					<div className="bar">
+						<Bar data={generateChartData()} options={options} />
+					</div>
+				</>
+			) : (
+				<Backdrop
+					sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+					open={!conditionReducer.predictions}
+				>
+					<CircularProgress color="inherit" />
+				</Backdrop>
+			)}
+		</>
+	);
 }
 export default Prediction;
-
